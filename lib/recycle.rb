f3621855c78acdd5f7ddd63adfd2b5fac6e9e4cb
rfaerbz1 <bfaerber@cox.net>
2018-06-09 14:36:06 +0000

    automatically backed up by learn

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -5,25 +5,28 @@
 # We haven't hard coded the hashes in the tests, 
 #they're dynamically brought in from the NYC Open Data API
 
-first_recycle_bin =  {
-  "address"=>"E 227 St/Bronx River Pkway",
-  "site_type"=>"Subproperty",
-  "longitude"=>"-73.864223918",
-  "borough"=>"Bronx",
-  "latitude"=>"40.890848989",
-  "park_site_name"=>"227th St. Plgd"
-}
+# first_recycle_bin =  {
+#   "address"=>"E 227 St/Bronx River Pkway",
+#   "site_type"=>"Subproperty",
+#   "longitude"=>"-73.864223918",
+#   "borough"=>"Bronx",
+#   "latitude"=>"40.890848989",
+#   "park_site_name"=>"227th St. Plgd"
+# }
 
 
 #1. Define a method `address`, that takes a hash as an argument.
 #This method should return the value of the address key in the hash.
 def address (hash)
-  puts hash["address"]
+  hash["address"]
 end 
-address(first_recycle_bin)
+
 
 #2. Define a method called `borough` that takes a hash as an argument.
 # This method should return the value of the borough key in the hash.
+def address (borough)
+  hash["borough"]
+end 
 
 #3. Define a method called `all_keys` that takes a hash as an argument.
 #This method should print out each key in the hash. 
