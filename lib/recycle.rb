d2d234bfcbfb39637fe2eac4f690be75b0eeacb8
rfaerbz1 <bfaerber@cox.net>
2018-06-09 14:34:41 +0000

    automatically backed up by learn

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -18,7 +18,7 @@
 #1. Define a method `address`, that takes a hash as an argument.
 #This method should return the value of the address key in the hash.
 def address (hash)
-  hash.address
+  hash["address"]
 end 
 address(first_recycle_bin)
 
